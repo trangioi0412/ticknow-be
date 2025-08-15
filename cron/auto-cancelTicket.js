d4dea71d268074f772dd6d9233696556242ddd97
trangioi0412 <trangioi479@gmail.com>
2025-08-16 01:04:53 +0700

    bugfix: api update status movie

--- cron/auto-cancelTicket.js
+++ cron/auto-cancelTicket.js
@@ -3,7 +3,7 @@ const cron = require('node-cron');
 const ticketModel = require('../model/ticket.model');
 const ticketService = require('../service/ticket.service');
 
-cron.schedule('* * * * *', async () => {
+cron.schedule('*/1 * * * * *', async () => {
     const now = new Date();
     const expiredOrders = await ticketModel.find({
         type: 1,
